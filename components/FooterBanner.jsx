110528bf4246716d3dccd7e73f8f9df41b391d69
paulvalderama <pvalderama@gmail.com>
2022-10-27 15:45:38 -0700

    remove buttons

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -17,9 +17,9 @@ const FooterBanner = ({ footerBanner: {discount, largeText1, largeText2, saleTim
           <p>{smallText}</p>
           <h3>{midText}</h3>
           <p>{desc}</p>
-          <Link href={`/product/${product}`}>
+          {/* <Link href={`/product/${product}`}>
             <button type="button">{buttonText}</button>
-          </Link>
+          </Link> */}
         </div>
         <img
           src={urlFor(image)}
